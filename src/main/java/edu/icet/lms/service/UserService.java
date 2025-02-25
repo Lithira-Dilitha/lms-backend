7dbc920ef2a2818ccaebfa2d32b8d991e6473302
Lithira Dilitha <157275775+Lithira-Dilitha@users.noreply.github.com>
2025-02-25 09:39:27 +0530

    use jdbc template for native query

--- src/main/java/edu/icet/lms/service/UserService.java
+++ src/main/java/edu/icet/lms/service/UserService.java
@@ -2,8 +2,12 @@ package edu.icet.lms.service;
 
 import edu.icet.lms.model.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
-    User rejisterUser(User user);
-    User findByEmail(String email);
-    User update(User user);
+    int rejisterUser(User user);
+    Optional<User> findByEmail(String email);
+    int update(User user);
+    List<User> allUsers();
 }
